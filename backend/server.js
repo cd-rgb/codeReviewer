aaf4715b86fada0eac6487a0c825e2778064f32b
xing24xing <khushipandey260304@gmail.com>
2025-02-12 02:54:12 +0530

    Fix the url related issues

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,25 @@
-require('dotenv').config()
-// const app = require('./src/app')
-
+require('dotenv').config();
 const express = require('express');
-const aiRoutes = require('./src/routes/ai.routes')
-const cors = require('cors')
-const path =require('path')
-const app = express()
+const aiRoutes = require('./src/routes/ai.routes');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
 const PORT = process.env.PORT || 8000;
-const __dirname = path.resolve();
 
-//middlewares
+// Middlewares
 app.use(express.json());
-app.use(cookieParser());
-app.use(urlencoded({ extended: true }));
-const corsOptions = {
-    origin: process.env.URL,
-    credentials: true
-}
-app.use(cors(corsOptions));
+app.use(cors({ origin: process.env.URL, credentials: true }));
 
+// Routes
+app.use('/ai', aiRoutes);
 
-app.use('/ai', aiRoutes)
-
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-app.get('*', (_, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+// Serve Frontend
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
 });
+
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
